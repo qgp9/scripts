f19b2f526dbbd1f68a4a34e4c206f8c3164c3a25
qgp9 <qgp@skepties.net>
2016-11-17 23:11:29 +0200

    Update Install_local_perl_lib.sh

--- Install_local_perl_lib.sh
+++ Install_local_perl_lib.sh
@@ -23,4 +23,4 @@ echo "## Install Loca::Lib"
 $MYPERL/bin/cpanm -l $MYPERL local::lib
 
 echo "## add 'myperl' to ~/.bash_profile"
-echo 'export MYPERL=$HOME/perl5; myperl () { :;S=${1-mydev};eval `perl -I $MYPERL/$S/lib/perl5 -Mlocal::lib=$MYPERL/$S`; }' >> ~/.bash_profile
+echo 'export MYPERL=$HOME/perl5; myperl () { S=${1-mydev};eval `perl -I $MYPERL/$S/lib/perl5 -Mlocal::lib=$MYPERL/$S`; }' >> ~/.bash_profile
