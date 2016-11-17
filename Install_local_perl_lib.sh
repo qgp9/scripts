f6727065dabf37b8354cf404fd272dce080b37fc
qgp9 <qgp@skepties.net>
2016-11-17 23:06:50 +0200

    Create Install_local_perl_lib.sh

---
+++ Install_local_perl_lib.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Manage perl5 library under user directory by name
+#
+# This scripts install
+# 
+# 1. cpanm
+# 2. Local::Lib
+#
+# Under $HOME/perl5/mydev
+#
+# `myperl` on a command line will give you a localize perl environment
+# 
+# Later you can set isolated libs like; `myperl mojo`, `myperl dev2`
+
+
+export MYPERL=$HOME/perl5/mydev
+
+echo "## Install CPANM"
+curl -L http://cpanmin.us | perl -I $MYPERL - -f -l $MYPERL App::cpanminus
+
+echo "## Install Loca::Lib"
+$MYPERL/bin/cpanm -l $MYPERL local::lib
+
+echo "## add 'myperl' to ~/.bash_profile"
+echo 'export MYPERL=$HOME/perl5; myperl () { :;S=${1-mydev};eval `perl -I $MYPERL/$S/lib/perl5 -Mlocal::lib=$MYPERL/$S`; }' >> ~/.bash_profile
