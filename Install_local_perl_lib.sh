b5593482aa47b2e0fff9c29e3d486b91317a28c8
qgp9 <qgp@skepties.net>
2016-11-17 23:26:34 +0200

    Update Install_local_perl_lib.sh

--- Install_local_perl_lib.sh
+++ Install_local_perl_lib.sh
@@ -13,19 +13,26 @@
 # 
 # Later you can set isolated libs like; `myperl mojo`, `myperl dev2`
 
-
+echo
 echo "## Make library Set 'myperl' at $HOME/perl5/myperl"
 echo "## This is template dictory to generate library set in easy."
 echo "## Hence, Don't use this lib set directly."
+echo
 export MYPERL=$HOME/perl5/myperl
 
+echo
 echo "### Install CPANM"
+echo
 curl -L http://cpanmin.us | perl -I $MYPERL - -f -l $MYPERL App::cpanminus
 
+echo
 echo "### Install Loca::Lib"
+echo
 $MYPERL/bin/cpanm -l $MYPERL local::lib
 
+echo
 echo "## Make library set 'mydev' at $HOME/perl5/mydev:" 
+echo
 cp -a $MYPERL $MYPERL/../mydev
 
 
